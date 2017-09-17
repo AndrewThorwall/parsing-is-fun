4103e4abae5ae12d14c4bee46fa1db4f630c69e5
Andrew Thorwall <anthor@umich.edu>
2017-09-16 23:14:45 -0700

    nfas

--- nfa.hs
+++ nfa.hs
@@ -1,47 +1,125 @@
+import Data.List
 import qualified Data.Map as Map
 
 -- This type is not equipped to deal with NFAs in general--only NFAs that are the 
 -- Thomson construction of some regular expression.
 
-data Symbol = Epsilon | Symbol Char deriving (Show, Read, Eq, Ord)
+data Symbol = Epsilon 
+            | Symbol Char deriving (Show, Read, Eq, Ord)
+
+data NfaState = NfaState Integer deriving (Show, Read, Eq, Ord)
+
+unNfaState :: NfaState -> Integer
+unNfaState (NfaState x) = x
+
+shiftNfaState :: Integer -> NfaState -> NfaState
+shiftNfaState n (NfaState s) = NfaState $ s + n
+
+data NfaTransition = NfaTransition (Map.Map Symbol [NfaState]) deriving (Show, Read, Eq)
+
+unNfaTransition :: NfaTransition -> Map.Map Symbol [NfaState]
+unNfaTransition (NfaTransition m) = m
+
+emptyNfaTransition :: NfaTransition
+emptyNfaTransition = NfaTransition (Map.fromList [])
+
+addEdgeToTransition :: Symbol -> NfaState -> NfaTransition -> NfaTransition
+addEdgeToTransition symbol state (NfaTransition n) = NfaTransition $
+    Map.insertWith (union) symbol [state] n
+
+shiftNfaTransition :: Integer -> NfaTransition -> NfaTransition
+shiftNfaTransition n (NfaTransition t) = NfaTransition $
+    Map.map (map (\(NfaState x) -> NfaState $ x + n)) t
 
 data Nfa = Nfa 
-    { accept :: Bool
-    , transition :: Map.Map Symbol [Nfa]
+    { states :: Map.Map NfaState NfaTransition
+    , initial :: NfaState
+    , final :: NfaState
     } deriving (Show, Read, Eq)
 
-data NfaConstruction = NfaConstruction
-    { initial :: Nfa
-    , final :: Nfa
-    } deriving (Show, Read, Eq)
+getNewState :: Nfa -> NfaState
+getNewState (Nfa { states = s }) 
+    | length (Map.keys s) == 0 = NfaState 0
+    | otherwise                = NfaState ((unNfaState $ maximum (Map.keys s)) + 1)
 
-emptyState :: Bool -> Nfa
-emptyState accept = Nfa { accept = accept
-                        , transition = Map.fromList []
-                        }
+addState :: Nfa -> NfaState -> Nfa
+addState (Nfa { states = n, initial = i, final = f }) s = Nfa 
+    { states = Map.insert s emptyNfaTransition n
+    , initial = i
+    , final = f
+    } 
 
-addEdge :: Nfa -> Symbol -> Nfa -> Nfa
-addEdge nfa symbol target = Nfa (Map.insertWith (++) symbol [target] (transition nfa))
+addEdge :: Symbol -> NfaState -> NfaState -> Nfa -> Nfa
+addEdge symbol s1 s2 (Nfa { initial = i, states = n, final = f }) = Nfa 
+    { states = Map.adjust (addEdgeToTransition symbol s2) s1 n 
+    , initial = i
+    , final = f
+    }
 
-emptyConstruction :: NfaConstruction
-emptyConstruction = NfaConstruction { initial = addEdge (emptyState False) Epsilon final
-                                    , final = final
-                                    }
-    where final = emptyState 
+-- Given a symbol, creates a NFA consisting of an initial state and a final state,
+-- and a transition from the initial state to the final state for that symbol.
+literalConstruction :: Symbol -> Nfa
+literalConstruction s = Nfa
+    { states = Map.fromList [(NfaState 0, 
+                              NfaTransition $ Map.fromList [(s,[NfaState 1])]), 
+                             (NfaState 1, 
+                              NfaTransition $ Map.fromList [])]
+    , initial = NfaState 0
+    , final = NfaState 1
+    }
 
--- Adds a null edge from the final state to the initial state, creates a new final state, and adds
--- null edges from the initial state and old final state to the new final state.
---kleeneStarNfa :: NfaComponent -> Nfa
---kleeneStarNfa nfa = addEdge (final nfa) Epsilon (initial nfa)
+shiftNfa :: Integer -> Nfa -> Nfa
+shiftNfa n nfa = Nfa
+    { states = ((Map.mapKeys (shiftNfaState n)) . (Map.map (shiftNfaTransition n))) (states nfa)
+    , initial = NfaState $ unNfaState (initial nfa) + n
+    , final = NfaState $ unNfaState (final nfa) + n
+    }
 
--- Adds a null edge between all accepting states in the first NFA and the initial state of the second NFA. 
---connectNfas :: Nfa -> Nfa -> Nfa
---connectNfas nfa1 nfa2 = connectNfasRecursive nfa1 nfa2 []
+combineNfas :: Nfa -> Nfa -> (Nfa, Integer)
+combineNfas nfa1 nfa2 = (nfa, shift)
+    where nfa = Nfa 
+              { states = Map.union (states nfa1) (states nfa2')
+              , initial = initial nfa1
+              , final = final nfa1
+              }
+          nfa2' = shiftNfa shift nfa2
+          shift = unNfaState (getNewState nfa1)
 
---connectNfasRecursive :: Nfa -> Nfa -> [Nfa] -> Nfa
---connectNfasRecursive nfa1 nfa2 seen = Nfa { initial = initial nfa1
---                                          , transition 
---                                          }
+-- Given two NFAs, creates a new NFA by adding an empty edge from the final state of the first
+-- NFA to the initial state of the second NFA. State keys will be updated to avoid collisions.
+concatenateNfas :: Nfa -> Nfa -> Nfa
+concatenateNfas nfa1 nfa2 = addEdge Epsilon (final nfa1) shiftedInitial adjusted
+    where (combined, shift) = combineNfas nfa1 nfa2
+          shiftedInitial = shiftNfaState shift $ initial nfa2
+          shiftedFinal = shiftNfaState shift $ final nfa2
+          adjusted = Nfa
+              { states = states combined
+              , initial = initial nfa1 
+              , final = shiftedFinal 
+              }
 
---postfixRegexToNfa :: String -> Nfa
---postfixRegexToNfa regex = 
+-- Performs an NFA step WITHOUT following empty edges (unless symbol is Epsilon). 
+oneStepNfa :: Nfa -> Symbol -> NfaState -> [NfaState]
+oneStepNfa nfa symbol state 
+    | Map.member symbol t = t Map.! symbol
+    | otherwise = []
+    where t = unNfaTransition $ (states nfa) Map.! state
+
+oneEpsilonStepNfa :: Nfa -> NfaState -> [NfaState]
+oneEpsilonStepNfa nfa state = union [state] (oneStepNfa nfa Epsilon state)
+
+
+closure :: (Eq a) => [a] -> (a -> [a]) -> [a]
+closure state transition = closureRec state transition Nothing
+
+closureRec :: (Eq a) => [a] -> (a -> [a]) -> Maybe [a] -> [a]
+closureRec state transition Nothing = closureRec (nub $ state >>= transition) transition (Just state)
+closureRec state transition (Just lastState)
+    | state == lastState = state
+    | otherwise = closureRec (nub $ state >>= transition) transition (Just state)
+
+epsilonClosure :: Nfa -> NfaState -> [NfaState]
+epsilonClosure nfa state = closure [state] (oneEpsilonStepNfa nfa)
+
+stepNfa :: Nfa -> Symbol -> NfaState -> [NfaState]
+stepNfa nfa symbol state = nub $ (oneStepNfa nfa symbol state) >>= (epsilonClosure nfa)
